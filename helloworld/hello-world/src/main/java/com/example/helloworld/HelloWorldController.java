4bcab20eadc556db7b2758c1dfc9087337237725
Sarath kumar Reddy <sarathkumarreddy@Saraths-MacBook-Pro.local>
2021-05-09 13:06:26 -0700

    added helloworldservice with simple virtual service and gateway ingress

---
+++ helloworld/hello-world/src/main/java/com/example/helloworld/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.example.helloworld;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping("/hello")
+	public ResponseEntity<?> healthCheckFail() {
+		return new ResponseEntity("Hello World!!!", HttpStatus.OK);
+
+	}
+
+}
